75da8004b1b53e2b64ae2be100bce7875d65721b
hanwo <hanwo2052@gmail.com>
2022-07-25 23:51:49 +0900

    신고결과받기 by hg

---
+++ src/com/wooseok/algorithm/programmers/level1/kakao_recruit_신고결과받기.java
@@ -0,0 +1,55 @@
+package com.wooseok.algorithm.programmers.level1;
+
+import java.util.*;
+
+/**
+ * 신입사원 무지는 게시판 불량 이용자를 신고하고 처리 결과를 메일로 발송하는 시스템을 개발하려 합니다. 무지가 개발하려는 시스템은 다음과 같습니다.
+ *
+ * 각 유저는 한 번에 한 명의 유저를 신고할 수 있습니다.
+ **     신고 횟수에 제한은 없습니다. 서로 다른 유저를 계속해서 신고할 수 있습니다.
+ **     한 유저를 여러 번 신고할 수도 있지만, 동일한 유저에 대한 신고 횟수는 1회로 처리됩니다.
+ * k번 이상 신고된 유저는 게시판 이용이 정지되며, 해당 유저를 신고한 모든 유저에게 정지 사실을 메일로 발송합니다.
+ **     유저가 신고한 모든 내용을 취합하여 마지막에 한꺼번에 게시판 이용 정지를 시키면서 정지 메일을 발송합니다.
+ */
+public class kakao_recruit_신고결과받기 {
+
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, Set<String>> reportTarget = new HashMap<>();
+        Map<String, Integer> reportOwner = new HashMap<>();
+
+        for (String s : id_list) {
+            reportTarget.put(s, new HashSet<>());
+            reportOwner.put(s, 0);
+        }
+        for (String str : report) {
+            String[] strings = str.split(" ");
+            reportTarget.get(strings[1]).add(strings[0]);
+        }
+
+        for (String key : id_list) {
+            Set<String> reportList = reportTarget.get(key);
+            if (reportList.size() >= k) {
+                for (String ownerKey : reportList) {
+                    reportOwner.put(ownerKey, reportOwner.get(ownerKey) + 1);
+                }
+            }
+        }
+        for (int i = 0; i < id_list.length; i++) {
+            answer[i] = reportOwner.get(id_list[i]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k = 2;
+        int[] solution = solution(id_list, report, 2);
+        for (int i : solution) {
+            System.out.println(i);
+        }
+    }
+}
