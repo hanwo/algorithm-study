79d3b7ca19e69ecaa19029f204720c0f8b670b24
hanwo <hanwo2052@gmail.com>
2022-08-02 21:20:45 +0900

    크레인 인형 뽑기 문제 준비

---
+++ src/com/wooseok/algorithm/programmers/level1/kakao_크레인인형뽑기.java
@@ -0,0 +1,14 @@
+package com.wooseok.algorithm.programmers.level1;
+
+public class kakao_크레인인형뽑기 {
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args){
+        int[][] board = {{0,0,0,0,0}, {0,0,1,0,3}, {0,2,5,0,1}, {4,2,4,4,2}, {3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
+        solution(board,moves);
+    }
+}
