f60fc79020ba8ecde4d96e7e16f9bf94a73c76e8
TLC 한우석 <ws.han@twolinecode.com>
2024-06-05 21:26:53 +0900

    구명보트

---
+++ src/com/wooseok/algorithm/programmers/algorithm/greedy/day17/middler/구명보트.java
@@ -0,0 +1,21 @@
+package com.wooseok.algorithm.programmers.algorithm.greedy.day17.middler;
+
+public class 구명보트 {
+
+    public static void main(String[] args) {
+        int[] people1 = {70, 50, 80, 50};
+        int[] people2 = {70,80,50};
+        int limit1 = 100;
+
+        int solution1 = solution(people1, limit1);
+        int solution2 = solution(people2, limit1);
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+
+    public static int solution(int[] people, int limit) {
+        int answer = 0;
+        return answer;
+    }
+}
