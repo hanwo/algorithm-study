16350fc05c1e4261885ff4b40361d4187b7e176b
hanwo <hanwo2052@gmail.com>
2024-06-06 11:37:12 +0900

    [leetCode]Counting_Bit

---
+++ src/com/wooseok/algorithm/programmers/algorithm/bruteForce/dfs/day14/Invert_Binary_Tree.java
@@ -0,0 +1,53 @@
+package com.wooseok.algorithm.programmers.algorithm.bruteForce.dfs.day14;
+
+public class Invert_Binary_Tree {
+
+    public static void main(String[] args) {
+//        4,2,7,1,3,6,9
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        TreeNode treeNode = invertTree(root);
+        System.out.println("treeNode = " + treeNode);
+
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ src/com/wooseok/algorithm/programmers/algorithm/dynamicprogramming/day18/Counting_Bits.java
@@ -0,0 +1,2 @@
+package com.wooseok.algorithm.programmers.algorithm.dynamicprogramming.day18;public class Counting_Bits {
+}
