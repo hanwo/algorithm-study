48142d743474589ed624d9d1d19180c3f2147dd1
hanwo <hanwo2052@gmail.com>
2024-06-15 23:04:38 +0900

    [leetcode]Number_of_Good_Pairs

---
+++ src/com/wooseok/algorithm/programmers/algorithm/array/day27/Number_of_Good_Pairs.java
@@ -0,0 +1,34 @@
+package com.wooseok.algorithm.programmers.algorithm.array.day27;
+
+import java.util.Arrays;
+
+public class Number_of_Good_Pairs {
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 3, 1, 1, 3};
+        int[] nums2 = {1, 1, 1, 1};
+        int[] nums3 = {1, 2, 3};
+
+        System.out.println("numIdenticalPairs(nums1) = " + numIdenticalPairs(nums1)); // 4
+        System.out.println("numIdenticalPairs(nums2) = " + numIdenticalPairs(nums2)); // 6
+        System.out.println("numIdenticalPairs(nums3) = " + numIdenticalPairs(nums3)); // 0
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        int answer = 0;
+
+        int[] copyArray = Arrays.copyOf(nums, nums.length);
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < copyArray.length; j++) {
+                if (nums[i] == copyArray[j]) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+
+    }
+}
