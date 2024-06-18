e6b52b43a9e7752089add60a03ad3a5f7e895c55
hanwo <hanwo2052@gmail.com>
2024-06-18 17:27:01 +0900

    lin: 린트 수정

--- src/com/wooseok/algorithm/programmers/algorithm/sort/day7/K번째수.java
+++ src/com/wooseok/algorithm/programmers/algorithm/sort/day7/K번째수.java
@@ -3,39 +3,40 @@ package com.wooseok.algorithm.programmers.algorithm.sort.day7;
 import java.util.Arrays;
 
 public class K번째수 {
-    public static void main(String[] args) {
-        int[] array = {1, 5, 2, 6, 3, 7, 4};
-        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+	public static void main(String[] args) {
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
-        int[] answer = solution(array, commands);
-        int[] answer2 = solution2(array, commands);
+		int[] answer = solution(array, commands);
+		int[] answer2 = solution2(array, commands);
 
-        System.out.println("answer = " + Arrays.toString(answer));
-        System.out.println("answer = " + Arrays.toString(answer2));
-    }
+		System.out.println("answer = " + Arrays.toString(answer));
+		System.out.println("answer = " + Arrays.toString(answer2));
+	}
 
-    static int[] solution(int[] array, int[][] commands) {
-        return Arrays.stream(commands)
-                .mapToInt(command -> {
-                    int i = command[0] - 1;
-                    int j = command[1];
-                    int k = command[2] - 1;
-                    int[] ints = Arrays.copyOfRange(array, i, j);
-                    Arrays.sort(ints);
-                    int anInt = ints[k];
-                    return anInt;
-                })
-                .toArray();
-    }
+	static int[] solution(int[] array, int[][] commands) {
+		return Arrays.stream(commands)
+			.mapToInt(command -> {
+				int i = command[0] - 1;
+				int j = command[1];
+				int k = command[2] - 1;
+				int[] ints = Arrays.copyOfRange(array, i, j);
+				Arrays.sort(ints);
+				int anInt = ints[k];
+				return anInt;
+			})
+			.toArray();
+	}
 
-    static int[] solution2(int[] array, int[][] commands) {
-        return Arrays.stream(commands)
-                .mapToInt(command -> {
-                    int[] slicedArray = Arrays.copyOfRange(array, command[0] - 1, command[1]);
-                    Arrays.sort(slicedArray);
-                    return slicedArray[command[2] - 1];
-                })
-                .toArray();
-    }
+	static int[] solution2(int[] array, int[][] commands) {
+		Arrays.sort(array);
+		return Arrays.stream(commands)
+			.mapToInt(command -> {
+				int[] slicedArray = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+				Arrays.sort(slicedArray);
+				return slicedArray[command[2] - 1];
+			})
+			.toArray();
+	}
 
 }
